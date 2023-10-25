ff3f91a2b840b9a8a09c033216c11232749a10fb
netka99 <anetaajduk@wp.pl>
2023-10-25 10:39:25 +0200

    prices settings updated

--- prices.js
+++ prices.js
@@ -1,4 +1,29 @@
 let pricesAmended = {};
+let shopsAmended = [];
+
+//replacing data
+async function updateSettings(shops, prices) {
+  try {
+    const response = await fetch(
+      'https://smacznykaseksuwalki.com/api/settings/aneta',
+      {
+        method: 'PUT',
+        headers: {
+          'Content-Type': 'application/json',
+        },
+        body: JSON.stringify({
+          shops,
+          prices,
+        }),
+      }
+    );
+    const data = await response.json();
+    console.log('Data:', data);
+  } catch (error) {
+    console.error('Error:', error);
+    throw error;
+  }
+}
 
 async function fetchData(url) {
   try {
@@ -20,24 +45,37 @@ async function loadPrices() {
       'https://smacznykaseksuwalki.com/api/settings/aneta'
     );
     pricesAmended = pricesData.prices;
+    shopsAmended = pricesData.shops;
     populatePrices(pricesAmended);
-    console.log(pricesAmended);
   } catch (error) {
     console.log('Error:', error);
   }
 }
 
 loadPrices();
+const kartaczeInput = document.getElementById('Price_Kartacze');
+const babkaInput = document.getElementById('Price_Babka');
+const kiszkaInput = document.getElementById('Price_Kiszka');
 
 const populatePrices = (data) => {
-  const kartaczeInput = document.getElementById('Price_Kartacze');
-  const babkaInput = document.getElementById('Price_Babka');
-  const kiszkaInput = document.getElementById('Price_Kiszka');
-
   kartaczeInput.value = data.Kartacze;
   babkaInput.value = data.Babka;
   kiszkaInput.value = data.Kiszka;
 };
+
+const savePrice = document.getElementById('savePrices');
+
+savePrice.addEventListener('click', () => {
+  pricesAmended.Kartacze = parseInt(kartaczeInput.value);
+  pricesAmended.Babka = parseInt(babkaInput.value);
+  pricesAmended.Kiszka = parseInt(kiszkaInput.value);
+
+  updateSettings(shopsAmended, pricesAmended);
+  savePrice.textContent = 'Zapisano';
+  kartaczeInput.disabled = true;
+  babkaInput.disabled = true;
+  kiszkaInput.disabled = true;
+});
 
 // // Example usage:
 // const newShops = [
