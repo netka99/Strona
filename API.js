593b38f834fc76776a0b2e46d13cf3bb10bdfcaa
netka99 <anetaajduk@wp.pl>
2023-10-27 11:20:04 +0200

    updates

---
+++ API.js
@@ -0,0 +1,4 @@
+const APISettings = 'https://smacznykaseksuwalki.com/api/settings/aneta';
+const APIGeneral = 'https://smacznykaseksuwalki.com/api';
+const APISales = 'https://www.smacznykaseksuwalki.com/api/sales';
+const APIReturns = 'https://www.smacznykaseksuwalki.com/api/returns';
